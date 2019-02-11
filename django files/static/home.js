0d53a25720811d41e984c5cfcc2f31536d8458fa
Arun Shrivastava <arunshrivastava112@gmail.com>
2019-02-12 01:22:50 +0530

    added more pages

--- django files/static/home.js
+++ django files/static/home.js
@@ -53,22 +53,22 @@ $('.dropdown-item').hover(
         });
       });
 
-     /*Scroll to top when arrow up clicked BEGIN*/
+     
+/*Scroll to top when arrow up clicked BEGIN*/
 $(window).scroll(function() {
-  var height = $(window).scrollTop();
-  if (height > 100) {
-      $('#back2Top').fadeIn();
-  } else {
-      $('#back2Top').fadeOut();
-  }
-});
-$(document).ready(function() {
-  $("#back2Top").click(function(event) {
-      event.preventDefault();
-      $("html, body").animate({ scrollTop: 0 }, "slow");
-      return false;
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
   });
-
-});
-/*Scroll to top when arrow up clicked END*/
-
+  $(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+  
+  });
+  /*Scroll to top when arrow up clicked END*/
