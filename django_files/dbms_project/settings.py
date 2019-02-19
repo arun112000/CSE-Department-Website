7de488972d61edebf4c381482da29ff0f40ccf56
Arun Shrivastava <arunshrivastava112@gmail.com>
2019-02-19 10:29:01 +0530

    minor changes

--- django_files/dbms_project/settings.py
+++ django_files/dbms_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ly6)$$+)lz7k2jl89+m3kv!uyhy-y=gp88x)+5@gpkmk4(jl)6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.137.1','192.168.137.219','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.137.1','192.168.137.219','127.0.0.1','http://arun112000.pythonanywhere.com/']
 
 
 # Application definition
